63b5fd5a495bd670aa0e0acbce674e804eaa3be7
Danialefar <enc_danielfarias@hotmail.com.ar>
2023-10-24 20:28:04 -0300

    24/10 daniel

--- src/Nutricionista_G32_accesoDatos/Dieta_Comida_Data.java
+++ src/Nutricionista_G32_accesoDatos/Dieta_Comida_Data.java
@@ -5,14 +5,19 @@
  */
 package Nutricionista_G32_accesoDatos;
 
+import Nutricionista_G32_entidades.Comida;
 import Nutricionista_G32_entidades.Dieta;
 import Nutricionista_G32_entidades.Dieta_Comida;
+import Nutricionista_G32_entidades.Paciente;
+import Nutricionista_G32_entidades.TiposComida;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 
@@ -49,5 +54,50 @@ public class Dieta_Comida_Data {
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA DIETA_COMIDA" + ex.getMessage());
         }
+    }
+     
+     public List<Dieta_Comida> listarDetalleComidas(int id) {
+        List<Dieta_Comida> dietaCom = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM dieta_comida, comida, dieta "
+                    + "WHERE dieta_comida.id_dieta=? "
+                    + "AND dieta.id_dieta=? "
+                    + "AND dieta_comida.id_comida=comida.id_comida";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.setInt(2, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Comida comida = new Comida();
+                Dieta dieta =  new Dieta();
+                Dieta_Comida dC = new Dieta_Comida();
+                Paciente_Data pD = new Paciente_Data();
+                
+                comida.setId_comida(rs.getInt("id_comida"));
+                comida.setNombre_comida(rs.getString("nombre_comida"));
+                comida.setDetalle_comida(rs.getString("detalle_comida"));
+                comida.setCalorias_comida(rs.getInt("calorias_comida"));
+                comida.setEstado_comida(rs.getBoolean("estado_comida"));
+                
+                dieta.setId_dieta(rs.getInt("id_dieta"));
+                dieta.setNombre_dieta(rs.getString("nombre_dieta"));
+                Paciente pac = pD.buscarPacientePorId(rs.getInt("id_paciente"));
+                dieta.setPaciente(pac);
+                dieta.setPeso_incial(rs.getDouble("peso_inicial"));
+                dieta.setPeso_final(rs.getDouble("peso_final"));
+                dieta.setFecha_inicial(rs.getDate("fecha_inicial").toLocalDate());
+                dieta.setFecha_final(rs.getDate("fecha_final").toLocalDate());
+                
+                dC.setId_dieta_comida(rs.getInt("id_dieta_comida"));
+                dC.setComida(comida);
+                dC.setDieta(dieta);
+                dC.setTipo_comida(TiposComida.valueOf(rs.getString("tipo_comida")));
+                dietaCom.add(dC);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA COMIDA" + ex.getMessage());
+        }
+        return dietaCom;
     }
 }
